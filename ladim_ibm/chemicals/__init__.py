6677813a6e4abbf5fe951ce7767afe6c5c2408ae
Pål Næverlid Sævik <a5606@hi.no>
2020-02-07 13:12:39 +0100

    Add IBM

--- ladim_ibm/chemicals/__init__.py
+++ ladim_ibm/chemicals/__init__.py
@@ -1,1 +1,2 @@
 from .gridforce import Grid, Forcing
+from .ibm import IBM
---
+++ ladim_ibm/chemicals/ibm.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class IBM:
+    def __init__(self, config):
+        self.D = config["ibm"].get('vertical_mixing', 1e-3)  # Vertical mixing [m*2/s]
+        self.dt = config['dt']
+
+    def update_ibm(self, grid, state, _):
+        # Vertical diffusion velocity
+        rand = np.random.normal(size=len(state.X))
+        W = rand * (2 * self.D / self.dt) ** 0.5
+
+        # Update vertical position, using reflexive boundary condition at top
+        state.Z += W * self.dt
+        state.Z[state.Z < 0] *= -1
+
+        # Reflexive boundary condition at bottom
+        H = grid.sample_depth(state.X, state.Y)  # Water depth
+        below_seabed = state.Z > H
+        state.Z[below_seabed] = 2*H[below_seabed] - state.Z[below_seabed]
