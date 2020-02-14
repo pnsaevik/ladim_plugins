032c7b72f2696b09dd8c6d1c8b723f7fb92c4562
Pål Næverlid Sævik <a5606@hi.no>
2020-02-14 12:08:08 +0100

    Add polygon sampling functions

--- ladim_plugins/sedimentation/make_release.py
+++ ladim_plugins/sedimentation/make_release.py
@@ -1,2 +1,131 @@
-def main():
+import numpy as np
+import xarray as xr
+
+
+def main(
+    location=None, start_time='2000-01-01', stop_time='2000-01-01',
+    num_particles=0,
+):
+    # noinspection PyPackageRequirements
+    import pandas as pd
+
+    release = pd.DataFrame(
+        columns=['active', 'release_time', 'lat', 'lon', 'Z', 'sink_vel', 'group_id'])
+
+    # Handle default arguments
+    if location is None:
+        location = dict(lat=[0, 0, 0], lon=[0, 0, 0])
+
     return ""
+
+
+# noinspection PyPackageRequirements
+def get_release_data(date, lat, lon, z, distribution, num_particles):
+    np.random.seed(0)
+
+    from shapely.wkt import loads as load_wkt
+
+    distribution_shape = load_wkt(distribution)
+    if distribution_shape.wkt == 'POINT (0 0)':
+        lat_arr = np.repeat(lat, num_particles)
+        lon_arr = np.repeat(lon, num_particles)
+    elif distribution_shape.wkt.startswith('POLYGON'):
+        from oceanviz.crs import transform as crs_transform
+        coords = np.array(distribution_shape.exterior.xy).T
+        x, y = get_polygon_sample(coords, num_particles)
+        lat_arr, lon_arr = crs_transform(x, y, ('local', lat, lon), 'ETRS89')
+    else:
+        raise ValueError(f'Distribution "{distribution_shape.wkt}" is unknown')
+
+    try:
+        zmin = z[0]
+        zmax = z[1]
+        z_arr = zmin + np.random.rand(num_particles) * (zmax - zmin)
+    except TypeError:
+        z_arr = np.repeat(z, num_particles)
+
+    date_arr = np.repeat(np.datetime64(date), num_particles)
+
+    return xr.Dataset(
+        data_vars=dict(
+            release_time=xr.Variable(
+                dims='particle',
+                data=date_arr,
+            ),
+            lat=xr.Variable(
+                dims='particle',
+                data=lat_arr,
+            ),
+            lon=xr.Variable(
+                dims='particle',
+                data=lon_arr,
+            ),
+            Z=xr.Variable(
+                dims='particle',
+                data=z_arr,
+            ),
+        )
+    )
+
+
+def _unit_triangle_sample(num):
+    xy = np.random.rand(num*2).reshape((2, -1))
+    is_upper_triangle = np.sum(xy, axis=0) > 1
+    xy[:, is_upper_triangle] = 1 - xy[:, is_upper_triangle]
+    return xy
+
+
+# noinspection PyPackageRequirements
+def get_polygon_sample(coords, num):
+    from shapely.geometry import Polygon
+    from shapely.ops import triangulate
+    np.random.seed(0)
+
+    # Triangulate the polygon
+    poly = Polygon(coords)
+    areas = []
+    subcoords = []
+    for t in triangulate(poly):
+        areas.append(t.area)
+        subcoords.append(t.exterior.coords.xy)
+
+    # Distribute the points proportionally among the different triangles
+    cumarea = np.cumsum(areas)
+    triangle_num = np.searchsorted(cumarea / cumarea[-1], np.random.rand(num))
+
+    # Sample within the triangles
+    s, t = _unit_triangle_sample(num)
+    (x1, y1), (x2, y2), (x3, y3), _ = np.array(subcoords)[triangle_num].T
+    x = (x2 - x1) * s + (x3 - x1) * t + x1
+    y = (y2 - y1) * s + (y3 - y1) * t + y1
+    return x, y
+
+
+# noinspection PyPackageRequirements
+def get_concave_polygon_sample(coords, num):
+    import triangle as tr
+    from shapely.geometry import Polygon
+    np.random.seed(0)
+
+    # Triangulate the polygon
+    sequence = list(range(len(coords)))
+    trpoly = dict(vertices=coords,
+                  segments=np.array((sequence, sequence[1:] + [0])).T)
+    trdata = tr.triangulate(trpoly, 'p')
+    subcoords = []
+    areas = []
+    for tidx in trdata['triangles']:
+        t = Polygon(trdata['vertices'][tidx])
+        subcoords.append(t.exterior.coords.xy)
+        areas.append(t.area)
+
+    # Distribute the points proportionally among the different triangles
+    cumarea = np.cumsum(areas)
+    triangle_num = np.searchsorted(cumarea / cumarea[-1], np.random.rand(num))
+
+    # Sample within the triangles
+    s, t = _unit_triangle_sample(num)
+    (x1, y1), (x2, y2), (x3, y3), _ = np.array(subcoords)[triangle_num].T
+    x = (x2 - x1) * s + (x3 - x1) * t + x1
+    y = (y2 - y1) * s + (y3 - y1) * t + y1
+    return x, y
--- ladim_plugins/sedimentation/test_make_release.py
+++ ladim_plugins/sedimentation/test_make_release.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ladim_plugins.sedimentation import make_release
 
 
@@ -6,3 +7,103 @@ class Test_main:
         config = dict()
         result = make_release.main(**config)
         assert isinstance(result, str)
+
+    def test_accepts_params(self):
+        config = dict(
+            location=dict(
+                lat=[0],
+                lon=[0],
+            )
+        )
+        result = make_release.main(**config)
+        assert isinstance(result, str)
+
+
+class Test_get_release_data:
+    def test_is_homogeneous_table_when_point_input(self):
+        data = make_release.get_release_data(
+            date='2019-01-02T03:04:05', lat=60, lon=5, z=1,
+            distribution='POINT (0 0)', num_particles=10)
+
+        assert list(data.variables) == ['release_time', 'lat', 'lon', 'Z']
+        assert list(data.dims) == ['particle']
+        assert data.dims['particle'] == 10
+        assert data.lat.values.tolist() == [60] * 10
+        assert data.lon.values.tolist() == [5] * 10
+        assert data.Z.values.tolist() == [1] * 10
+        assert data.release_time.values.astype(str).tolist() == \
+            ['2019-01-02T03:04:05.000000000'] * 10
+
+    def test_varying_zcoord_when_tuple_z_input(self):
+        data = make_release.get_release_data(
+            date='2019-01-02T03:04:05', lat=60, lon=5, z=(1, 5),
+            distribution='POINT (0 0)', num_particles=10)
+
+        z = data.Z.values
+
+        assert z.min() > 1
+        assert z.max() < 5
+        assert len(np.unique(z)) == len(z)
+
+    def test_horizontal_distribution_when_polygon_input(self):
+        data = make_release.get_release_data(
+            date='2019-01-02T03:04:05', lat=60, lon=0, z=0, num_particles=1000,
+            distribution='POLYGON ((-1 -1, 1 -1, 1 1, -1 1, -1 -1))')
+
+        lat_m = (data.lat.values - 60) * 60 * 1852
+        lon_m = data.lon.values * 60 * 1852 * 0.5
+
+        assert -1 < lat_m.min() < -0.9
+        assert 0.9 < lat_m.max() < 1
+        assert -1 < lon_m.min() < -0.9
+        assert 0.9 < lon_m.max() < 1
+
+
+class Test_get_polygon_sample:
+    def test_all_inside_when_triangle(self):
+        coords = np.array([[5, 3], [5, 1], [6, 3]])
+        x, y = make_release.get_polygon_sample(coords, 100)
+
+        assert np.all(x >= 5)
+        assert np.all(y <= 3)
+        assert np.all(2 * (y - 1) >= (x - 5))
+
+    def test_all_inside_when_rectangle(self):
+        coords = np.array([[1, 10], [2, 10], [2, 12], [1, 12]])
+        x, y = make_release.get_polygon_sample(coords, 100)
+
+        assert np.all(x >= 1)
+        assert np.all(x <= 2)
+        assert np.all(y >= 10)
+        assert np.all(y <= 12)
+
+    def test_does_not_work_when_nonconvex_polygon(self):
+        coords = np.array([[0, 0], [10, 0], [10, 10], [9, 1]])
+        x, y = make_release.get_polygon_sample(coords, 100)
+        is_inside_forbidden_area = (x < 9) & (y > 1)
+        assert np.count_nonzero(is_inside_forbidden_area) > 0
+
+
+class Test_get_concave_polygon_sample:
+    def test_all_inside_when_triangle(self):
+        coords = np.array([[5, 3], [5, 1], [6, 3]])
+        x, y = make_release.get_concave_polygon_sample(coords, 100)
+
+        assert np.all(x >= 5)
+        assert np.all(y <= 3)
+        assert np.all(2 * (y - 1) >= (x - 5))
+
+    def test_all_inside_when_rectangle(self):
+        coords = np.array([[1, 10], [2, 10], [2, 12], [1, 12]])
+        x, y = make_release.get_concave_polygon_sample(coords, 100)
+
+        assert np.all(x >= 1)
+        assert np.all(x <= 2)
+        assert np.all(y >= 10)
+        assert np.all(y <= 12)
+
+    def test_works_when_nonconvex_polygon(self):
+        coords = np.array([[0, 0], [10, 0], [10, 10], [9, 1]])
+        x, y = make_release.get_concave_polygon_sample(coords, 100)
+        is_inside_forbidden_area = (x < 9) & (y > 1)
+        assert np.count_nonzero(is_inside_forbidden_area) == 0
