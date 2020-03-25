83e99bd5ac2131295b0b096b1e0895c17f78fda1
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-03-25 14:16:35 +0100

    Refactor: Extract internal functions

--- ladim_plugins/sedimentation/ibm.py
+++ ladim_plugins/sedimentation/ibm.py
@@ -13,9 +13,39 @@ class IBM:
         # Store time step value to calculate age
         self.dt = config['dt']
 
+        # Reference to other modules
+        self.grid = None
+        self.forcing = None
+        self.state = None
+
     def update_ibm(self, grid, state, forcing):
+        self.grid = grid
+        self.forcing = forcing
+        self.state = state
+
+        self.sink_particles()
+        self.bury_particles()
+        self.kill_old_particles()
+
+    def bury_particles(self):
+        grid = self.grid
+        a = self.state.active != 0
+        X, Y, Z = self.state.X[a], self.state.Y[a], self.state.Z[a]
+
+        # Define which particles have settled to the bottom and which have not
+        H = grid.sample_depth(X, Y)  # Water depth
+        at_seabed = Z > H
+        Z[at_seabed] = H[at_seabed]
+
+        # Store new vertical position
+        self.state.Z[a] = Z
+        self.state.active[a] = ~at_seabed
+
+    def sink_particles(self):
+        state = self.state
+
         a = state.active != 0
-        X, Y, Z = state.X[a], state.Y[a], state.Z[a]
+        Z = state.Z[a]
 
         # Read sinking velocity for settling particles
         W = state.sink_vel[a]
@@ -29,15 +59,10 @@ class IBM:
         Z += W * self.dt
         Z[Z < 0] *= -1
 
-        # Define which particles have settled to the bottom and which have not
-        H = grid.sample_depth(X, Y)  # Water depth
-        at_seabed = Z > H
-        Z[at_seabed] = H[at_seabed]
-
         # Store new vertical position
         state.Z[a] = Z
-        state.active[a] = ~at_seabed
 
-        # Aging and particle death
+    def kill_old_particles(self):
+        state = self.state
         state.age += state.dt
         state.alive = state.alive & (state.age <= self.lifespan)
