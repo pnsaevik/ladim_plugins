5e6cae8c3f6ccd6770ee21ae8fd6a54c35614b16
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-03-26 13:22:14 +0100

    Document that euler is not well-mixed

--- ladim_plugins/sedimentation/test_ibm.py
+++ ladim_plugins/sedimentation/test_ibm.py
@@ -33,3 +33,44 @@ class Test_sde_solver_euler:
         sol = ibm.sde_solver(x0, t0, advect_fn, diffuse_fn, dt, 'euler')
 
         assert sol.tolist() == [[2, 4, 6], [5, 7, 9]]
+
+    def test_exact_when_linear_onedim(self):
+        x0 = np.array([1, 2, 3])
+        t0 = 0
+        dt = 1
+
+        def advect_fn(x, _):
+            return np.ones_like(x)
+
+        def diffuse_fn(x, _):
+            return np.zeros_like(x)
+
+        sol = ibm.sde_solver(x0, t0, advect_fn, diffuse_fn, dt, 'euler')
+
+        assert sol.tolist() == [2, 3, 4]
+
+    def test_not_well_mixed(self):
+        np.random.seed(0)
+        x0 = np.linspace(0, 1, 100)
+        t0 = 0
+        dt = 1
+        numsteps = 100
+
+        def advect_fn(x, _):
+            return np.zeros_like(x)
+
+        def diffuse_fn(x, _):
+            return x
+
+        sol = x0
+        t = t0
+        for i in range(numsteps):
+            sol = ibm.sde_solver(sol, t, advect_fn, diffuse_fn, dt, 'euler')
+            # Reflective boundaries
+            sol[sol < 0] *= -1
+            sol[sol > 1] = 2 - sol[sol > 1]
+
+        # Check distribution
+        hist_num, hist_edges = np.histogram(sol, bins=[0, .5, 1])
+        assert hist_num[0] > len(x0) * .9  # More entries is low-diffusive region
+        assert hist_num[1] < len(x0) * .1  # Less entries in high-diffusive region
