e6b42e161e431a385dbf987b78d57370f9c4c842
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-04-01 11:52:00 +0200

    Cosmetic changes

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
     license='MIT Licence',
     author='Pål Næverlid Sævik',
     author_email='a5606@hi.no',
-    description='IBMs for LADiM', install_requires=['numpy', 'pytest', 'xarray',
-                                                    'PyYAML', 'netCDF4'],
+    description='IBMs for LADiM',
+    install_requires=[
+        'numpy', 'pytest', 'xarray', 'PyYAML', 'netCDF4', 'shapely',
+    ],
 )
