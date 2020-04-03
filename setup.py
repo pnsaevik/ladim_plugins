257607dbc2e6785c49fe2a62f2bbb1fa31ec1385
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-04-03 08:22:43 +0200

    Add ladim to requirements

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     description='IBMs for LADiM',
     install_requires=[
         'numpy', 'pytest', 'xarray', 'PyYAML', 'netCDF4',
-        'triangle', 'scipy', 'cftime',
+        'triangle', 'scipy', 'cftime', 'ladim',
     ],
 )
