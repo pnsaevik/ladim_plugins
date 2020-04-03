d8a46945e6121aecf464e2379fcf589dae8e205f
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-04-03 13:03:46 +0200

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,35 @@ setup(
     packages=find_packages(),
     package_data={'': ['*.nc', '*.md', '*.rls', '*.yaml', '*.m']},
     url='https://github.com/pnsaevik/ladim_plugins',
-    license='MIT Licence',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+
+        "Operating System :: OS Independent",
+    ],
     author='Pål Næverlid Sævik',
     author_email='a5606@hi.no',
-    description='IBMs for LADiM',
+    description='Plugins for LADiM',
     install_requires=[
         'numpy', 'pytest', 'xarray', 'PyYAML', 'netCDF4',
         'triangle', 'scipy', 'cftime', 'ladim',
     ],
+    python_requires='>=3.6',
 )
