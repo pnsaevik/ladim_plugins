7bf996b6dd0075167a784b10011429dccb934567
Pål Næverlid Sævik <a5606@hi.no>
2020-06-04 08:47:19 +0200

    Rename methods
    The shorter name is sufficient

--- ladim_plugins/release/__init__.py
+++ ladim_plugins/release/__init__.py
@@ -1,3 +1,3 @@
 from .makrel import make_release
-from .farms import polygon as get_farm_polygon
-from .farms import location as get_farm_location
+from .farms import polygon as farm_polygon
+from .farms import location as farm_location
