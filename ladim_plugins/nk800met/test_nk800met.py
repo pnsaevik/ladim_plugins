ceb59b5ad261fb42a352e280b53b364b2093be2a
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2020-07-03 13:31:18 +0200

    Add buffered forcing class

--- ladim_plugins/nk800met/test_nk800met.py
+++ ladim_plugins/nk800met/test_nk800met.py
@@ -1,17 +1,18 @@
+import numpy as np
 from ladim_plugins.nk800met import gridforce
 
 
 class Test_OnlineDatabase_request_dset:
     def test_can_get_metadata(self):
-        time = '2020-01-01T12:30'
+        time = np.datetime64('2020-01-01T12:30')
         dbase = gridforce.OnlineDatabase()
-        dset = dbase.request_dset(time)
+        dset = dbase.get_dset(time)
         assert 'depth' in dset.dimensions.keys()
         assert 'w' in dset.variables.keys()
         assert 'Conventions' in dset.ncattrs()
 
     def test_can_get_metadata_when_async(self):
-        time = '2020-01-01T12:30'
+        time = np.datetime64('2020-01-01T12:30')
         dbase = gridforce.OnlineDatabase()
 
         def check(dset):
