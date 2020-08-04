03a7ed80b0a02efee256fca0f3b524dc7f1aa0af
Pål Næverlid Sævik <a5606@hi.no>
2020-08-04 08:26:58 +0200

    Add snapshot test to local test module
    For easier isolated testing of the benthos module

---
+++ ladim_plugins/benthos/test_benthos.py
@@ -0,0 +1,10 @@
+import xarray as xr
+from ladim_plugins.tests import test_examples as tex
+
+
+def test_output_matches_snapshot():
+    module_name = 'benthos'
+    out = tex.run_ladim(module_name)
+    # out.to_netcdf(get_module_dir(module_name).joinpath('out.nc'))
+    ref = xr.load_dataset(tex.get_module_dir(module_name).joinpath('out.nc'))
+    tex.check_equal(out, ref)
