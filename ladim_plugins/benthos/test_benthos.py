9ba5da36278ce2627e8ffc5eb15220cb5dfa1923
Pål Næverlid Sævik <a5606@hi.no>
2020-08-04 13:31:35 +0200

    Remove make_release from benthos package

--- ladim_plugins/benthos/test_benthos.py
+++ ladim_plugins/benthos/test_benthos.py
@@ -1,6 +1,5 @@
 import xarray as xr
 from ladim_plugins.tests import test_examples as tex
-from ladim_plugins.benthos import make_release
 
 
 def test_output_matches_snapshot():
@@ -9,14 +8,3 @@ def test_output_matches_snapshot():
     # out.to_netcdf(get_module_dir(module_name).joinpath('out.nc'))
     ref = xr.load_dataset(tex.get_module_dir(module_name).joinpath('out.nc'))
     tex.check_equal(out, ref)
-
-
-class Test_make_release:
-    def test_returns_dataframe(self):
-        config = dict(
-
-        )
-
-        from pandas import DataFrame
-        result = make_release.main(config)
-        assert isinstance(result, DataFrame)
