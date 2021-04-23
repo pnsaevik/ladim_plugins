f39c4a726faa781e2ba75404b0629e55d6da2a3d
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2021-04-23 14:59:12 +0200

    Add weight to output

--- ladim_plugins/larvae/test_ibm.py
+++ ladim_plugins/larvae/test_ibm.py
@@ -44,8 +44,8 @@ class Test_weight_to_length:
         assert L.tolist() == [5.108959666553063, 9.934365412170125, 18.295002538937236]
 
 
-# def test_snapshot():
-#    import ladim_plugins.tests.test_examples
-#    import os
-#    os.chdir(os.path.dirname(ladim_plugins.tests.test_examples.__file__))
-#    ladim_plugins.tests.test_examples.test_output_matches_snapshot('larvae')
+def test_snapshot():
+    import ladim_plugins.tests.test_examples
+    import os
+    os.chdir(os.path.dirname(ladim_plugins.tests.test_examples.__file__))
+    ladim_plugins.tests.test_examples.test_output_matches_snapshot('larvae')
