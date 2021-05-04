aa6af56a557d3fe11a413de05492dad35ae84121
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2021-05-04 07:24:56 +0200

    Replace sampler with scipy map_coordinate

--- ladim_plugins/sedimentation/gridforce.py
+++ ladim_plugins/sedimentation/gridforce.py
@@ -5,6 +5,12 @@ class Grid(ladim.gridforce.ROMS.Grid):
     def __init__(self, config):
         super().__init__(config)
 
+    def sample_depth(self, X, Y):
+        from scipy.ndimage import map_coordinates
+        i = X - self.i0
+        j = Y - self.j0
+        return map_coordinates(self.H, [j, i], order=0)
+
 
 class Forcing(ladim.gridforce.ROMS.Forcing):
     def __init__(self, config, grid):
