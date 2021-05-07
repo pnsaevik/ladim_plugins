c135a4fc30c667cc9eb6f987aee92a2fa7433d09
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2021-05-07 09:47:25 +0200

    Add larval development and vertical mixing

--- ladim_plugins/shrimp/ibm.py
+++ ladim_plugins/shrimp/ibm.py
@@ -1,12 +1,25 @@
+import numpy as np
+
+
 class IBM:
 
     def __init__(self, config):
-        self.state = None
-        self.grid = None
-        self.forcing = None
+        stage_duration = config['ibm'].get('stage_duration', 12)  # [days]
+        self.devel_rate = 1 / (stage_duration * 24 * 60 * 60)  # [s^-1]
+
+        self.vertical_mixing = config['ibm'].get('vertical_mixing', 0)  # [m2/s]
+
         self.dt = config['dt']
 
     def update_ibm(self, grid, state, forcing):
-        self.state = state
-        self.grid = grid
-        self.forcing = forcing
+        # --- Larval development ---
+        state['age'] += self.dt
+        state['stage'] += self.dt * self.devel_rate
+        state['stage'] = np.maximum(state['stage'], 6)  # 6 is the maximum stage
+        state['active'] = state['stage'] < 6  # Stage 6 does not move with the currents
+
+        # --- Vertical turbulent mixing ---
+        if self.vertical_mixing:
+            dw = np.random.normal(size=len(state.X))
+            dz = np.sqrt(2 * self.vertical_mixing * self.dt) * dw
+            state['Z'] += dz
