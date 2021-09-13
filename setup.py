5fbef7e8a9e70118f38468f10000b997da5c4ffd
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2021-09-13 09:03:26 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ladim_plugins',
-    version='1.5.6',
+    version='1.5.7',
     packages=find_packages(),
     package_data={'': [
         '*.nc', '*.md', '*.rls', '*.yaml', '*.m', '*.bsp', '*.geojson',
