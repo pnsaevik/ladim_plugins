97d083fd44b67266c2ff8e6226892d4e3fda347f
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2021-12-03 09:21:23 +0100

    Add MultiDataset class

--- ladim_plugins/utils/rasterize.py
+++ ladim_plugins/utils/rasterize.py
@@ -566,6 +566,327 @@ def get_weight_func(spec):
     return weight_fn
 
 
+class MultiDataset:
+    """Encapsulates a collection of netCDF4 datasets.
+
+    The class contains a main dataset which holds all the coordinate variables.
+    Some of the coordinate variables are defined as "cross-dataset"
+    coordinates. Variables having any of the cross-dataset dimensions are
+    spread across multiple datasets. Variables with only "regular" coordinates
+    are kept in the main dataset.
+    """
+
+    def __init__(self, filename, **kwargs):
+        """
+        Initialize a MultiDataset. The file of the main dataset is given
+        explicitly. The names of the subdatasets are constructed from the main
+        filename, of the form "maindatasetname_firstcoordvalue_secondcoordvalue".
+
+        :param filename: Name of the main dataset.
+        :param kwargs: Additional arguments passed to netCDF4.Dataset
+        """
+        self._dataset_kwargs = kwargs
+        self.main_dataset = nc.Dataset(filename, mode='w', **kwargs)
+        self.datasets = dict()
+        self._cross_coords = dict()
+        self._cross_vars = dict()
+        self._editable = True  # When subdatasets are created, no more variables can be added
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        self.main_dataset.close()
+        for d in self.datasets.values():
+            d.close()
+
+    def createCoord(self, varname, data, attrs=None, cross_dataset=False):
+        """
+        Create coordinate variable and dimension in the main dataset.
+
+        Coordinate variables cannot be created after setData or getData is used
+        on a cross-dataset variable.
+        
+        :param varname: Name of coordinate variable and dimension 
+        :param data: Data used to initialize variable
+        :param attrs: Optional coordinate attributes
+        :param cross_dataset: True if this is a cross-dataset dimension
+        :return: The newly created variable
+        """
+        if not self._editable:
+            raise TypeError("Adding new variables must be done before accessing cross-dataset data")
+
+        data = np.array(data)
+        dset = self.main_dataset
+        dset.createDimension(varname, len(data))
+        variable = dset.createVariable(varname, data.dtype, varname)
+        variable.set_auto_maskandscale(False)
+        variable[:] = data
+        if attrs:
+            dset.variables[varname].setncatts(attrs)
+        if cross_dataset:
+            self._cross_coords[varname] = data
+        return variable
+
+    def getCoord(self, varname):
+        """
+        Return a coordinate variable from the main dataset
+        :param varname: Variable name
+        :return: The variable object
+        """
+        return self.main_dataset.variables[varname]
+
+    def getAttrs(self, varname):
+        """
+        Return the attributes of either a reuglar or cross-dataset
+        variable.
+
+        :param varname: Variable name
+        :return: A dict of attributes
+        """
+
+        # Get cross-dataset variable attributes
+        if varname in self._cross_vars:
+            return self._cross_vars[varname]['attrs']
+
+        # Get regular variable attributes
+        else:
+            v = self.main_dataset.variables[varname]
+            return {k: v.getncattr(k) for k in v.ncattrs()}
+
+    def createVariable(self, varname, data, dims, attrs=None):
+        """
+        Create a dataset variable. If any of the dimensions are cross-dataset,
+        the variable will be spread out over multiple datasets.
+
+        Variables cannot be created after setData or getData is used on a
+        cross-dataset variable.
+
+        :param varname: Variable name
+        :param data: The initial data of the variable
+        :param dims: A tuple of dimension names
+        :param attrs: An optional dict of attributes
+        :return: The variable object if regular variable, None otherwise
+        """
+        if not self._editable:
+            raise TypeError("Adding new variables must be done before accessing cross-dataset data")
+
+        data = np.array(data)
+
+        # Create cross-dataset variable
+        if any(d in self._cross_coords for d in dims):
+            self._cross_vars[varname] = dict(
+                data=data,
+                dims=dims,
+                attrs=attrs or dict(),
+            )
+            variable = None
+
+        # Create regular variable
+        else:
+            dset = self.main_dataset
+            variable = dset.createVariable(varname, data.dtype, dims)
+            variable.set_auto_maskandscale(False)
+            variable[:] = data
+            if attrs:
+                variable.setncatts(attrs)
+
+        return variable
+
+    def _get_filename(self, file_idx):
+        import os
+        stubs = [str(v) for v in file_idx.values()]
+        base, ext = os.path.splitext(self.main_dataset.filepath())
+        return base + '_' + '_'.join(stubs) + ext
+
+    def _create_dset(self, file_idx):
+        """Return sub-dataset, or create it if necessary"""
+        file_idx_hash = tuple(file_idx.items())
+        if file_idx_hash not in self.datasets:
+            fname = self._get_filename(file_idx)
+            dset = nc.Dataset(fname, mode='w', **self._dataset_kwargs)
+            self._copy_from_main(dset, file_idx)
+            self.datasets[file_idx_hash] = dset
+            self._editable = False
+
+        return self.datasets[file_idx_hash]
+
+    def _copy_from_main(self, dest, crossdim_idx):
+        cross_dims = list(self._cross_coords.keys())
+        source = self.main_dataset
+
+        # Copy dimensions
+        for dim in source.dimensions.values():
+            if dim.name in cross_dims:
+                dest.createDimension(dim.name, 1)
+            else:
+                dest.createDimension(dim.name, dim.size)
+
+        # Copy variables
+        for v in source.variables.values():
+            new_var = dest.createVariable(v.name, v.dtype, v.dimensions)
+            new_var.set_auto_maskandscale(False)
+            atts = {k: v.getncattr(k) for k in v.ncattrs()}
+            new_var.setncatts(atts)
+            if v.name in cross_dims:
+                # If crossdim coordinate, copy only one value
+                global_idx = crossdim_idx[v.name]
+                new_var[:] = v[global_idx]
+                new_var.setncattr('global_index', global_idx)
+            else:
+                # Otherwise, copy all data
+                new_var[:] = v[:]
+
+        # Add filesplit variables
+        for vname, vinfo in self._cross_vars.items():
+            new_var = dest.createVariable(vname, vinfo['data'].dtype, vinfo['dims'])
+            new_var.set_auto_maskandscale(False)
+            new_var[:] = vinfo['data']
+            dest.variables[vname].setncatts(vinfo['attrs'])
+
+        # Copy global attributes
+        atts = {k: source.getncattr(k) for k in source.ncattrs()}
+        dest.setncatts(atts)
+
+    def _get_shape_of_variable_data(self, varname, idx):
+        shape = []
+        for dim_slice, dim_name in zip(idx, self._cross_vars[varname]['dims']):
+            dim_size = self.main_dataset.dimensions[dim_name].size
+            dim_range = range(*dim_slice.indices(dim_size))
+            shape.append(len(dim_range))
+        return tuple(shape)
+
+    def getData(self, varname, idx=None):
+        """
+        Get data from a dataset variable.
+
+        :param varname: Variable name
+        :param idx: A tuple of slices, or None if the entire data range is desired.
+        :return: A numpy array of data
+        """
+
+        if varname in self._cross_vars:
+            variable_info = self._cross_vars[varname]
+            if idx is None:
+                idx = (slice(None),) * len(variable_info['dims'])
+
+            shape = self._get_shape_of_variable_data(varname, idx)
+            data = np.empty(shape, variable_info['data'].dtype)
+
+            for i_file, i_var, i_data in self._split_indices(varname, idx):
+                dset = self._create_dset(i_file)
+                data[i_data] = dset.variables[varname][i_var]
+
+            return data
+
+        else:
+            if idx is None:
+                idx = slice(None)
+            return self.main_dataset.variables[varname][idx]
+
+    def setData(self, varname, data, idx=None):
+        """
+        Set data of a dataset variable
+
+        :param varname: Variable name
+        :param data: A numpy array of data
+        :param idx: A tuple of slices, or None if the entire data range is desired.
+        :return: The input data
+        """
+        if varname in self._cross_vars:
+            variable_info = self._cross_vars[varname]
+            if idx is None:
+                idx = (slice(None),) * len(variable_info['dims'])
+
+            shape = self._get_shape_of_variable_data(varname, idx)
+            bdata = np.broadcast_to(data, shape)
+
+            for i_file, i_var, i_data in self._split_indices(varname, idx):
+                dset = self._create_dset(i_file)
+
+                dset.variables[varname][i_var] = bdata[i_data]
+
+            return data
+
+        else:
+            if idx is None:
+                idx = slice(None)
+            self.main_dataset.variables[varname][idx] = data
+        return data
+
+    def incrementData(self, varname, data, idx=None):
+        """
+        Increment a dataset variable
+
+        :param varname: Variable name
+        :param data: A numpy array of data
+        :param idx: A tuple of slices, or None if the entire data range is desired.
+        :return: The input data
+        """
+        previous = self.getData(varname, idx)
+        newdata = previous + np.broadcast_to(previous.shape, data)
+        self.setData(varname, newdata, idx)
+
+    def _split_indices(self, varname, index):
+        """
+        Iterate over subdatasets to aid access of cross-dataset variables.
+
+        The input index is a tuple of slices, relative to the global shape
+        of the variable. The function iterates over each dataset spanned by
+        the range.
+
+        For each iteration, returns a tuple `(cross_idx, var_idx, data_idx)`
+        representing the portion of the data spanned by a single dataset.
+        - `cross_idx` is a dict of the form `{crossdimA: intA, crossdimB: intB, ...}`,
+          identifying the subdataset.
+        - `var_idx` is a tuple of either zeros or slices. For each
+          cross-dataset dimension there is a zero, and for each main dimension
+          there is a slice (taken directly from `index`). This tuple can be
+          used to directly index variables in a subdataset.
+        - `data_idx` is a tuple of slice(None) and integers. For each cross-
+          dataset dimension there is an integer, and for each main dimension
+          there is slice(None). The integer is relative to the shape of the
+          data subset.
+        :param varname: The variable name
+        :param index: A tuple of slices
+        :return: A tuple `(cross_idx, var_idx, data_idx)` representing a
+        portion of the data spanned by a single subdataset.
+        """
+
+        from itertools import product
+
+        varinfo = self._cross_vars[varname]
+        dims = varinfo['dims']
+        is_splitdim = [dim in self._cross_coords for dim in dims]
+        big_shape = [self.main_dataset.dimensions[d].size for d in dims]
+        big_range = [range(*i.indices(s)) for i, s in zip(index, big_shape)]
+        shape = [len(range(*i.indices(s))) for i, s in zip(index, big_shape)]
+
+        # Construct the var_idx, which is the index of the subdataset variable
+        var_idx = []
+        for dim, index_element, sd in zip(dims, index, is_splitdim):
+            if sd:
+                var_idx.append(0)
+            else:
+                var_idx.append(index_element)
+
+        # Construct the index elements relative to the big data array
+        data_indices = []
+        for index_element, dim, sz, sd in zip(index, dims, shape, is_splitdim):
+            if sd:
+                data_indices.append(range(sz))
+            else:
+                data_indices.append([slice(None)])
+
+        # Iterate over the cartesian product
+        for data_idx in product(*data_indices):
+            file_idx = {d: br[di] for d, br, di, sd in zip(dims, big_range, data_idx, is_splitdim) if sd}
+            yield file_idx, var_idx, data_idx
+
+
 class RasterOutputStream:
     def __init__(self, spec, coords, filesplit_dims=()):
         self.datasets = dict()
--- ladim_plugins/utils/test_rasterize.py
+++ ladim_plugins/utils/test_rasterize.py
@@ -3,6 +3,7 @@ import pytest
 import xarray as xr
 import numpy as np
 import netCDF4 as nc
+from uuid import uuid4
 
 
 @pytest.fixture(scope='module')
@@ -405,3 +406,135 @@ class Test_RasterOutputStream:
             assert r.dataset(X=3, Z=9).variables['X'][:].tolist() == [3]
             assert r.dataset(X=5, Z=9).variables['X'][:].tolist() == [5]
             assert r.dataset(X=5, Z=9).variables['Z'][:].tolist() == [9]
+
+
+class Test_MultiDataset:
+    @pytest.fixture(scope="function")
+    def mdset(self):
+        with rasterize.MultiDataset(uuid4()) as d:
+            yield d
+
+    def test_can_set_main_coordinate(self, mdset):
+        mdset.createCoord('mycoord', [1, 2, 3])
+        assert mdset.getCoord('mycoord')[:].tolist() == [1, 2, 3]
+
+    def test_can_set_main_variable(self, mdset):
+        mdset.createCoord('A', [1, 2, 3])
+        mdset.createCoord('B', [10, 20])
+        mdset.createVariable('myvar', np.arange(6).reshape((2, 3)), ('B', 'A'))
+        assert mdset.getData('myvar').tolist() == [[0, 1, 2], [3, 4, 5]]
+
+    def test_can_get_partial_data_of_main_variable(self, mdset):
+        mdset.createCoord('A', [1, 2, 3])
+        mdset.createCoord('B', [10, 20])
+        mdset.createVariable('myvar', np.arange(6).reshape((2, 3)), ('B', 'A'))
+
+        data = mdset.getData('myvar')
+        idx = np.s_[:1, 1:]
+        assert mdset.getData('myvar', idx).tolist() == data[idx].tolist()
+
+    def test_can_set_attrs_when_creating_main_coord_or_variable(self, mdset):
+        mdset.createCoord('mycoord', [1, 2, 3], attrs=dict(myatt=123))
+        mdset.createVariable('myvar', [4, 5, 6], 'mycoord', attrs=dict(myatt=9))
+        assert mdset.getAttrs('mycoord') == dict(myatt=123)
+        assert mdset.getAttrs('myvar') == dict(myatt=9)
+
+    def test_can_set_data_of_main_variable_after_creation(self, mdset):
+        mdset = rasterize.MultiDataset(uuid4())
+        mdset.createCoord('mycoord', [1, 2, 3])
+        mdset.createVariable('myvar', [4, 5, 6], 'mycoord')
+        mdset.setData('myvar', [7, 8, 9])
+        assert mdset.getData('myvar').tolist() == [7, 8, 9]
+
+    def test_can_set_partial_data_of_main_variable_after_creation(self, mdset):
+        mdset = rasterize.MultiDataset(uuid4())
+        mdset.createCoord('mycoord', [1, 2, 3])
+        mdset.createVariable('myvar', [4, 5, 6], 'mycoord')
+        mdset.setData('myvar', 7, idx=0)
+        assert mdset.getData('myvar').tolist() == [7, 5, 6]
+        mdset.setData('myvar', [0, 9], idx=np.s_[1:])
+        assert mdset.getData('myvar').tolist() == [7, 0, 9]
+
+    def test_can_set_initial_constant_data_of_multifile_variable_at_creation(self, mdset):
+        mdset.createCoord('A', [1, 2, 3])
+        mdset.createCoord('B', [4, 5], cross_dataset=True)
+
+        mdset.createVariable('x', 1, ('B', 'A'))
+        assert mdset.getData('x').tolist() == [[1, 1, 1], [1, 1, 1]]
+
+    def test_can_set_attrs_of_multifile_variable(self, mdset):
+        mdset.createCoord('A', [1, 2, 3])
+        mdset.createCoord('B', [4, 5], cross_dataset=True)
+        mdset.createVariable('x', 1, ('B', 'A'), attrs=dict(myatt=123))
+        assert mdset.getAttrs('x') == dict(myatt=123)
+
+    def test_can_set_initial_slicewise_data_of_multifile_variable_at_creation(self, mdset):
+        mdset.createCoord('A', [1, 2, 3])
+        mdset.createCoord('B', [4, 5], cross_dataset=True)
+
+        mdset.createVariable('x', np.arange(3).reshape((1, 3)), ('B', 'A'))
+        assert mdset.getData('x').tolist() == [[0, 1, 2], [0, 1, 2]]
+
+    def test_can_get_partial_data_of_multifile_variable(self, mdset):
+        mdset.createCoord('A', [1, 2, 3, 4])
+        mdset.createCoord('B', [5, 6, 7, 8, 9], cross_dataset=True)
+
+        mdset.createVariable('x', np.arange(4), ('B', 'A'))
+        assert mdset.getData('x', np.s_[1:3, 1:4]).tolist() == [[1, 2, 3], [1, 2, 3]]
+
+    def test_can_set_partial_data_of_multifile_variable_after_creation(self, mdset):
+        mdset.createCoord('A', [1, 2, 3])
+        mdset.createCoord('B', [5, 6, 7, 8], cross_dataset=True)
+
+        mdset.createVariable('x', 1, ('B', 'A'))
+        mdset.setData('x', [[8], [9]], np.s_[1:3, 1:2])
+        data = mdset.getData('x').tolist()
+        assert data == [[1, 1, 1], [1, 8, 1], [1, 9, 1], [1, 1, 1]]
+
+    def test_can_set_partial_data_when_multiple_crosscoords(self, mdset):
+        mdset.createCoord('A', [1, 2])
+        mdset.createCoord('B', [3, 4, 5], cross_dataset=True)
+        mdset.createCoord('C', [6, 7, 8, 9])
+        mdset.createCoord('D', [8, 7, 6, 5, 4], cross_dataset=True)
+
+        mdset.createVariable('x', 1, ('A', ))
+        mdset.createVariable('y', 2, ('B', 'A', ))
+        mdset.createVariable('z', 3, ('C', 'B', 'A', ))
+        mdset.createVariable('w', 4, ('D', 'C', 'B', 'A', ))
+
+        in_data = np.arange(6).reshape((1, 2, 3, 1))
+        idx = np.s_[1::5, :2, -3:, 0:1]
+        mdset.setData('w', in_data, idx)
+        out_data = mdset.getData('w')
+        assert out_data[idx].ravel().tolist() == in_data.ravel().tolist()
+
+    def test_cannot_create_variables_after_subdataset_creation(self, mdset):
+        # Create main-dataset coordinate (does not lock dataset)
+        mdset.createCoord('A', [1, 2])
+        # Create main-dataset variable (does not lock datset)
+        mdset.createVariable('x', 1, 'A')
+        # Set main-dataset variable data (does not lock dataset)
+        mdset.setData('x', 2)
+        # Create cross-dataset coordinate (does not lock dataset)
+        mdset.createCoord('B', [1, 2, 3], cross_dataset=True)
+        # Create cross-dataset variable (does not lock dataset)
+        mdset.createVariable('y', 3, 'B')
+        # Access cross-dataset variable (DOES lock dataset)
+        mdset.setData('y', 4)
+        # Cannot create new main-dataset coordinates when locked
+        with pytest.raises(TypeError):
+            mdset.createCoord('C', [1, 2])
+        # Cannot create new cross-dataset coordinates when locked
+        with pytest.raises(TypeError):
+            mdset.createCoord('D', [1, 2], cross_dataset=True)
+        # Cannot create new main-dataset variables when locked
+        with pytest.raises(TypeError):
+            mdset.createVariable('z', 5, 'A')
+        # Cannot create new cross-dataset variables when locked
+        with pytest.raises(TypeError):
+            mdset.createVariable('w', 6, 'B')
+        # CAN access variable data after locking
+        mdset.setData('x', 7)
+        assert mdset.getData('x').tolist() == [7, 7]
+        mdset.setData('y', 8)
+        assert mdset.getData('y').tolist() == [8, 8, 8]
