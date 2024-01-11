2ffd5afc46ac5ee438facaeb49bd5c090f390770
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2024-01-11 15:23:41 +0100

    Add vertical mixing extraction

--- ladim_plugins/salmon_lice/gridforce.py
+++ ladim_plugins/salmon_lice/gridforce.py
@@ -1,4 +1,5 @@
 import ladim.gridforce.ROMS
+from ladim.gridforce.ROMS import z2s, sample3D
 
 
 class Grid(ladim.gridforce.ROMS.Grid):
@@ -9,3 +10,10 @@ class Grid(ladim.gridforce.ROMS.Grid):
 class Forcing(ladim.gridforce.ROMS.Forcing):
     def __init__(self, config, grid):
         super().__init__(config, grid)
+
+    def vert_mix(self, X, Y, Z):
+        i0 = self._grid.i0
+        j0 = self._grid.j0
+        K, A = z2s(self._grid.z_w, X - i0, Y - j0, Z)
+        F = self['AKs']
+        return sample3D(F, X - i0, Y - j0, K, A, method="nearest")
