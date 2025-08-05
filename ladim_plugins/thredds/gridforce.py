1189981993c51c3e67405e8793907eb1bf0a099d
Pål Næverlid Sævik <a5606@hi.no>
2025-08-05 14:28:21 +0200

    Remove unused functions

--- ladim_plugins/thredds/gridforce.py
+++ ladim_plugins/thredds/gridforce.py
@@ -3,7 +3,6 @@ from netCDF4 import Dataset, num2date
 import glob
 import datetime
 import threading
-from ladim.gridforce import ROMS
 from ladim.sample import sample2D, bilin_inv
 import logging
 
@@ -12,18 +11,6 @@ logger = logging.getLogger(__name__)
 
 
 class Grid:
-    """Simple ROMS grid object
-
-    Possible grid arguments:
-      subgrid = [i0, i1, j0, j1]
-        Ordinary python style, start points included, not end points
-        Each of the elements can be replaced with None, for no limitation
-      Vinfo: dictionary with N, hc, theta_s and theta_b
-
-    """
-
-    # Lagrer en del unødige attributter
-
     def __init__(self, config):
 
         logger.info("Initializing ROMS-type grid object")
@@ -221,11 +208,6 @@ class Grid:
         return X + self.i0, Y + self.j0
 
 
-# -----------------------------------------------
-# The Forcing class from the old forcing module
-# -----------------------------------------------
-
-
 def get_fractional_index(sorted_array, search_value):
     idx_max = len(sorted_array)
     lower_unclipped = np.sum(sorted_array <= search_value) - 1
@@ -290,25 +272,24 @@ class ThreddsSource:
                 data = data.filled(0)
         return data
 
+
 class FieldCache:
     def __init__(self, src: ThreddsSource, varnames):
         self.source = src
         self.varnames = list(set(varnames))
         self.cache = dict()
-        self.block_shape = (-1, -1, -1, -1)
 
     def prepare(self, i, j, k, l):
         # The function loads required blocks into memory
         # At present, the ijk coordinates are disregarded
-        # It is also assumed that all time indices are the same
 
         # Delete old entries
         for time_block in self.cache.keys():
-            if time_block < int(l[0]):
+            if time_block < int(l):
                 del self.cache[time_block]
 
         # Load new entries
-        for time_block in [int(l[0]), int(l[0]) + 1]:
+        for time_block in [int(l), int(l) + 1]:
             if time_block in self.cache:
                 continue
 
@@ -433,11 +414,6 @@ class Transformer:
 
 
 class Forcing:
-    """
-    Class for ROMS forcing
-
-    """
-
     def __init__(self, config, _):
         logger.info("Initiating forcing")
 
@@ -472,274 +448,18 @@ class Forcing:
         )
 
         self._current_time_step = None
-        self._grid = grid  # Get the grid object, make private?
-        # self.config = config["gridforce"]
-        self.ibm_forcing = config["ibm_forcing"]
-
-        config = config.copy()
-        config["start_time"] = np.datetime64(config["start_time"])
-        config["stop_time"] = np.datetime64(config["stop_time"])
-        self._config = config
-
-    def _remaining_initialization(self):
-        steps = self.steps
-        V = [step for step in steps if step < 0]
-        if V:  # Forcing available before start time
-            prestep = max(V)
-            stepdiff = self.stepdiff[steps.index(prestep)]
-            nextstep = prestep + stepdiff
-            self.U, self.V = self._read_velocity(prestep)
-            self.Unew, self.Vnew = self._read_velocity(nextstep)
-            self.dU = (self.Unew - self.U) / stepdiff
-            self.dV = (self.Vnew - self.V) / stepdiff
-            # Interpolate to time step = -1
-            self.U = self.U - (prestep + 1) * self.dU
-            self.V = self.V - (prestep + 1) * self.dV
-            # Other forcing
-            for name in self.ibm_forcing:
-                self[name] = self._read_field(name, prestep)
-                self[name + "new"] = self._read_field(name, nextstep)
-                self["d" + name] = (self[name + "new"] - self[name]) / prestep
-                self[name] = self[name] - (prestep + 1) * self["d" + name]
-
-        elif steps[0] == 0:
-            # Simulation start at first forcing time
-            # Runge-Kutta needs dU and dV in this case as well
-            self.U, self.V = self._read_velocity(0)
-            self.Unew, self.Vnew = self._read_velocity(steps[1])
-            self.dU = (self.Unew - self.U) / steps[1]
-            self.dV = (self.Vnew - self.V) / steps[1]
-            # Synchronize with start time
-            self.Unew = self.U
-            self.Vnew = self.V
-            # Extrapolate to time step = -1
-            self.U = self.U - self.dU
-            self.V = self.V - self.dV
-            # Other forcing:
-            for name in self.ibm_forcing:
-                self[name] = self._read_field(name, 0)
-                self[name + "new"] = self._read_field(name, steps[1])
-                self["d" + name] = (self[name + "new"] - self[name]) / steps[1]
-                self[name] = self[name] - self["d" + name]
-
-        else:
-            # No forcing at start, should already be excluded
-            raise SystemExit(3)
-        self.has_been_initialized = True
-
-    # ===================================================
-    @staticmethod
-    def find_files(force_config):
-        """Find (and sort) the forcing file(s)"""
-
-        # Use unix-style filenames to provide consistency between windows and linux
-        first_file = force_config.get("first_file", "").replace("\\", "/")
-        last_file = force_config.get("last_file", "").replace("\\", "/")
-        files = [f.replace("\\", "/") for f in glob.glob(force_config["input_file"])]
-
-        files.sort()
-        if first_file:
-            files = [f for f in files if f >= first_file]
-        if last_file:
-            files = [f for f in files if f <= last_file]
-        return files
-
-    @staticmethod
-    def scan_file_times(files):
-        """Check files and scan the times
-
-        Returns:
-          all_frames: List of all time frames
-          num_frames: Mapping: filename -> number of time frames in file
-
-        """
-        all_frames = []  # All time frames
-        num_frames = {}  # Number of time frames in each file
-        for fname in files:
-            logger.info(f'Open forcing file {fname}')
-            with Dataset(fname) as nc:
-                new_times = nc.variables["ocean_time"][:]
-                num_frames[fname] = len(new_times)
-                units = nc.variables["ocean_time"].units
-                new_frames = num2date(new_times, units)
-                all_frames.extend(new_frames)
-
-        # Check that time frames are strictly sorted
-        all_frames = np.array([np.datetime64(tf) for tf in all_frames])
-        I = all_frames[1:] <= all_frames[:-1]
-        if np.any(I):
-            i = I.nonzero()[0][0] + 1   # Index of first out-of-order frame
-            oooframe = str(all_frames[i]).split('.')[0]  # Remove microseconds
-            logger.info(f"Time frame {i} = {oooframe} out of order")
-            logger.critical("Forcing time frames not strictly sorted")
-            raise SystemExit(4)
-
-        logger.info(f"Number of available forcing times = {len(all_frames)}")
-        return all_frames, num_frames
-
-    @staticmethod
-    def forcing_steps(config, files, all_frames, num_frames):
-
-        time0 = all_frames[0]
-        time1 = all_frames[-1]
-        logger.info(f"First forcing time = {time0}")
-        logger.info(f"Last forcing time = {time1}")
-        start_time = np.datetime64(config["start_time"])
-        dt = np.timedelta64(int(config["dt"]), "s")
-
-        # Check that forcing period covers the simulation period
-        # ------------------------------------------------------
-
-        if time0 > start_time:
-            logger.error("No forcing at start time")
-            raise SystemExit(3)
-        if time1 < config["stop_time"]:
-            logger.error("No forcing at stop time")
-            raise SystemExit(3)
-
-        # Make a list steps of the forcing time steps
-        # --------------------------------------------
-        steps = []  # Model time step of forcing
-        for t in all_frames:
-            dtime = np.timedelta64(t - start_time, "s").astype(int)
-            steps.append(int(dtime / config["dt"]))
-
-        file_idx = dict()  # Dårlig navn
-        frame_idx = dict()
-        step_counter = -1
-        # for i, fname in enumerate(files):
-        for fname in files:
-            for i in range(num_frames[fname]):
-                step_counter += 1
-                step = steps[step_counter]
-                file_idx[step] = fname
-                frame_idx[step] = i
-        return steps, file_idx, frame_idx
-
-    # ==============================================
-
+        self._grid = grid
 
     def update(self, t):
-        i, j, k, l = self.transformer.field_indices([0], [0], [0], [t])
+        i, j, k, l = self.transformer.field_indices([0], [0], [0], t)
         self.cache.prepare(i, j, k, l)
         self._current_time_step = t
         return
 
-
-        if not self.has_been_initialized:
-            self._remaining_initialization()
-
-        # Read from config?
-        interpolate_velocity_in_time = True
-        interpolate_ibm_forcing_in_time = False
-
-        logger.debug("Updating forcing, time step = {}".format(t))
-        if t in self.steps:  # No time interpolation
-            self.U = self.Unew
-            self.V = self.Vnew
-            for name in self.ibm_forcing:
-                self[name] = self[name + "new"]
-        else:
-            if t - 1 in self.steps:  # Need new fields
-                stepdiff = self.stepdiff[self.steps.index(t - 1)]
-                nextstep = t - 1 + stepdiff
-                self.Unew, self.Vnew = self._read_velocity(nextstep)
-                for name in self.ibm_forcing:
-                    self[name + "new"] = self._read_field(name, nextstep)
-                if interpolate_velocity_in_time:
-                    self.dU = (self.Unew - self.U) / stepdiff
-                    self.dV = (self.Vnew - self.V) / stepdiff
-                if interpolate_ibm_forcing_in_time:
-                    for name in self.ibm_forcing:
-                        self["d" + name] = (self[name + "new"] - self[name]) / stepdiff
-
-            # "Ordinary" time step (including self.steps+1)
-            if interpolate_velocity_in_time:
-                self.U += self.dU
-                self.V += self.dV
-            if interpolate_ibm_forcing_in_time:
-                for name in self.ibm_forcing:
-                    self[name] += self["d" + name]
-
-    # --------------
-
-    def open_forcing_file(self, n):
-        """Open forcing file at time step = n"""
-        nc = self._nc
-        nc = Dataset(self.file_idx[n])
-        nc.set_auto_maskandscale(False)
-
-        self.scaled = dict()
-        self.scale_factor = dict()
-        self.add_offset = dict()
-
-        # Åpne for alias til navn
-        forcing_variables = ["u", "v"] + self.ibm_forcing
-        for key in forcing_variables:
-            if hasattr(nc.variables[key], "scale_factor"):
-                self.scaled[key] = True
-                self.scale_factor[key] = np.float32(nc.variables[key].scale_factor)
-                self.add_offset[key] = np.float32(nc.variables[key].add_offset)
-            else:
-                self.scaled[key] = False
-
-        self._nc = nc
-
-    def _read_velocity(self, n):
-        """Read fields at time step = n"""
-        # Need a switch for reading W
-        # T = self._nc.variables['ocean_time'][n]  # Read new fields
-
-        # Handle file opening/closing
-        # Always read velocity before other fields
-        logger.info("Reading velocity for time step = {}".format(n))
-
-        # If finished a file or first read (self._nc == "")
-        if not self._nc:  # First read
-            self.open_forcing_file(n)
-        elif self.frame_idx[n] == 0:  # Just finished a forcing file
-            self._nc.close()
-            self.open_forcing_file(n)
-
-        frame = self.frame_idx[n]
-
-        # Read the velocity
-        U = self._nc.variables["u"][frame, :, self._grid.Ju, self._grid.Iu]
-        V = self._nc.variables["v"][frame, :, self._grid.Jv, self._grid.Iv]
-
-        # Scale if needed
-        # Assume offset = 0 for velocity
-        if self.scaled["u"]:
-            U = self.scale_factor["u"] * U
-            V = self.scale_factor["v"] * V
-            # U = self.add_offset['u'] + self.scale_factor['u']*U
-            # V = self.add_offset['v'] + self.scale_factor['v']*V
-
-        # If necessary put U,V = zero on land and land boundaries
-        # Stay as float32
-        np.multiply(U, self._grid.Mu, out=U)
-        np.multiply(V, self._grid.Mv, out=V)
-        return U, V
-
-    def _read_field(self, name, n):
-        """Read a 3D field"""
-        frame = self.frame_idx[n]
-        F = self._nc.variables[name][frame, :, self._grid.J, self._grid.I]
-        if self.scaled[name]:
-            F = self.add_offset[name] + self.scale_factor[name] * F
-        return F
-
-    # Allow item notation
-    def __setitem__(self, key, value):
-        setattr(self, key, value)
-
-    def __getitem__(self, key):
-        return getattr(self, key)
-
     def close(self):
         pass
 
-    def velocity(self, X, Y, Z, tstep=0, method="bilinear"):
+    def velocity(self, X, Y, Z, tstep=0):
         to_ijkl = self.transformer.field_indices
         i, j, k, l = to_ijkl(X, Y, Z, self._current_time_step + tstep)
         u = self.cache.sample(
@@ -758,7 +478,6 @@ class Forcing:
         )
         return u, v
 
-    # Simplify to grid cell
     def field(self, X, Y, Z, name):
         to_ijkl = self.transformer.field_indices
         i, j, k, l = to_ijkl(X, Y, Z, self._current_time_step)
@@ -906,57 +625,6 @@ def sdepth(H, Hc, C, stagger="rho", Vtransform=1):
 # ------------------------
 
 
-def z2s(z_rho, X, Y, Z):
-    """
-    Find s-level and coefficients for vertical interpolation
-
-    input:
-        z_rho  3D array with vertical s-coordinate structure at rho-points
-        X, Y   1D arrays, horizontal position in grid coordinates
-        Z      1D array, particle depth, meters, positive
-
-    Returns
-        K      1D integer array
-        A      1D float array
-
-    With:
-        1 <= K < kmax = z_rho.shape[0]
-        z_rho[K-1] < -Z < z_rho[K] for 1 < K < kmax - 1
-        -Z < z_rho[1] for K = 1
-        z_rho[-1] < -Z for K = kmax - 1
-        0.0 <= A <= 1
-        Interior linear interpolation:
-            A * z_rho[K - 1] + (1 - A) * z_rho[K] = -Z
-            for z_rho[0] < -Z < z_rho[-1]
-        Extend constant below lowest:
-            A * z_rho[K - 1] + (1 - A) * z_rho[K] = z_rho[0]
-            for -Z < z_rho[0]  (K=1, A=1)
-        Extend constantly above highest:
-            A * z_rho[K - 1] + (1 - A) * z_rho[K] = z_rho[-1]
-            for -Z > z_rho[-1]  (K=kmax-1, A=0)
-
-    """
-
-    kmax = z_rho.shape[0]  # Number of vertical levels
-
-    # Find rho-based horizontal grid cell (rho-point)
-    I = np.around(X).astype("int")
-    J = np.around(Y).astype("int")
-
-    # Constrain to valid indices
-    I = np.minimum(np.maximum(I, 0), z_rho.shape[-1] - 1)
-    J = np.minimum(np.maximum(J, 0), z_rho.shape[-2] - 1)
-
-    # Vectorized searchsorted
-    K = np.sum(z_rho[:, J, I] < -Z, axis=0)
-    K = K.clip(1, kmax - 1)
-
-    A = (z_rho[K, J, I] + Z) / (z_rho[K, J, I] - z_rho[K - 1, J, I])
-    A = A.clip(0, 1)  # Extend constantly
-
-    return K, A
-
-
 def z2s_frac(z_rho, i, j, z):
 
     kmax, jmax, imax = z_rho.shape
@@ -973,148 +641,3 @@ def z2s_frac(z_rho, i, j, z):
     frac = np.clip(frac_unclipped, 0, 1)
 
     return k + frac
-
-
-
-def sample3D(F, X, Y, K, A, method="bilinear"):
-    """
-    Sample a 3D field on the (sub)grid
-
-    F = 3D field
-    S = depth structure matrix
-    X, Y = 1D arrays of horizontal grid coordinates
-    Z = 1D array of depth [m, positive downwards]
-
-    Everything in rho-points
-
-    F.shape = S.shape = (kmax, jmax, imax)
-    S.shape = (kmax, jmax, imax)
-    X.shape = Y.shape = Z.shape = (pmax,)
-
-    # Interpolation = 'bilinear' for trilinear Interpolation
-    # = 'nearest' for value in 3D grid cell
-
-    """
-
-    if method == "bilinear":
-        # Find rho-point as lower left corner
-        I = X.astype("int")
-        J = Y.astype("int")
-
-        # Constrain to valid indices
-        I = np.minimum(np.maximum(I, 0), F.shape[-1] - 2)
-        J = np.minimum(np.maximum(J, 0), F.shape[-2] - 2)
-
-        P = X - I
-        Q = Y - J
-        W000 = (1 - P) * (1 - Q) * (1 - A)
-        W010 = (1 - P) * Q * (1 - A)
-        W100 = P * (1 - Q) * (1 - A)
-        W110 = P * Q * (1 - A)
-        W001 = (1 - P) * (1 - Q) * A
-        W011 = (1 - P) * Q * A
-        W101 = P * (1 - Q) * A
-        W111 = P * Q * A
-
-        return (
-            W000 * F[K, J, I]
-            + W010 * F[K, J + 1, I]
-            + W100 * F[K, J, I + 1]
-            + W110 * F[K, J + 1, I + 1]
-            + W001 * F[K - 1, J, I]
-            + W011 * F[K - 1, J + 1, I]
-            + W101 * F[K - 1, J, I + 1]
-            + W111 * F[K - 1, J + 1, I + 1]
-        )
-
-    # else:  method == 'nearest'
-    I = X.round().astype("int")
-    J = Y.round().astype("int")
-
-    # Constrain to valid indices
-    I = np.minimum(np.maximum(I, 0), F.shape[-1] - 1)
-    J = np.minimum(np.maximum(J, 0), F.shape[-2] - 1)
-
-    return F[K, J, I]
-
-
-def sample3DUV(U, V, X, Y, K, A, method="bilinear"):
-    return (
-        sample3D(U, X + 0.5, Y, K, A, method=method),
-        sample3D(V, X, Y + 0.5, K, A, method=method),
-    )
-
-
-class OnlineDatabase:
-    DEFAULT_URL = "https://thredds.met.no/thredds/dodsC/fou-hi/new_norkyst800m/his/ocean_his.an.{year:04}{month:02}{day:02}.nc"
-
-    def __init__(self, pattern=None):
-        self._dset_buf = Buffer()
-        self._vars_buf = Buffer()
-        self.pattern = pattern or self.DEFAULT_URL
-
-    def get_var(self, name, time):
-        val_1 = self._get_var(name, time)
-        val_2 = self._get_var(name, time + np.timedelta64(1, 'h'))
-        w = (time - time.astype('datetime64[h]')) / np.timedelta64(1, 'h')
-        return (val_1, val_2), w
-
-    def _get_var(self, name, time):
-        dset = self.get_dset(time)
-        tidx = time.astype(datetime.datetime).hour
-        tstr = str(time.astype('datetime64[h]'))
-        key = (name, tstr)
-        if key not in self._vars_buf:
-            v = dset[name][tidx, ...].filled(0)
-            self._vars_buf.push(key, v, tstr)
-        return self._vars_buf[key]
-
-    def get_dset(self, time):
-        t = time.astype(datetime.datetime)
-        tstr = str(time.astype('datetime64[D]'))
-        pat = self.pattern.format(year=t.year, month=t.month, day=t.day)
-        if pat not in self._dset_buf:
-            self._dset_buf.push(pat, nc.Dataset(pat), tstr)
-        return self._dset_buf[pat]
-
-    def request_dset(self, time, when_finished):
-        def request():
-            return when_finished(self.get_dset(time))
-        th = threading.Thread(target=request)
-        th.start()
-        return th
-
-    def close(self):
-        for dset in self._dset_buf.buf.values():
-            dset.close()
-        self._dset_buf = None
-
-    def __del__(self):
-        self.close()
-
-
-class Buffer:
-    def __init__(self):
-        max_frame_idx = 2
-        self.fidx_list = [None] * max_frame_idx
-        self.buf = dict()
-        self.fidx = dict()
-
-    def prune(self):
-        self.buf = {k: v for k, v in self.buf.items() if self.fidx[k] in self.fidx_list}
-        self.fidx = {k: v for k, v in self.fidx.items() if v in self.fidx_list}
-
-    def push(self, k, v, frame):
-        if frame not in self.fidx_list:
-            self.fidx_list = self.fidx_list[1:] + [frame]
-            self.prune()
-
-        self.buf[k] = v
-        self.fidx[k] = frame
-
-    def __getitem__(self, item):
-        return self.buf[item]
-
-    def __contains__(self, item):
-        return item in self.buf
-
